7a1976e14247c1564bb84355eb510bbb9481c6fe
user <yefferson.marin@gmail.com>
2015-12-21 22:05:20 -0500

    Some improvemments

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.site')
 @section('title', 'MigoHood | Alquila y alójate con anfitriones alrededor del Mundo')
 @section('header')
-  <link href="/css/site.css" rel="stylesheet">    <!-- Style core CSS -->                   
+  <link href="/css/site.css" rel="stylesheet">    <!-- Style core CSS -->
 @stop
 @section('content')
 <body id="home">
@@ -9,24 +9,29 @@
 <nav class="white" role="navigation">
   <div class="nav-wrapper">
     <!--<a href="#" data-activates="mobile" class="button-collapse"><i class="material-icons">dehaze</i></a>-->
-
-    <ul class=" hide-on-med-and-down">
+    <ul class="hide-on-med-and-down">
       <ul class="left">
-        <li><a href="{{ url('/') }}"><img id="brand" src="/img/logo.png" alt=".."/></a></li>
+        <li><a href="{{ url('/') }}"><img id="brand" src="/img/brand.png" alt=".."/></a></li>
       </ul>
       <ul class="right">
-        <li><a href="{{ url('auth/register') }}" class="btn btn-primary waves-effect waves-light">Sign up</a></li>
-        <li><a href="{{ url('auth/login') }}" class="btn btn-secundary waves-effect waves-light">Login</a></li>
+        <li><a href="" >Explorar</a></li>
+        <li><a href="" >¿Cómo ser Anfitrión?</a></li>
+        <li><a href="{{ url('') }}" class="btn btn-primary">Iniciar Sesión</a></li>
+        <li><a href="{{ url('') }}" class="btn btn-secundary waves-effect waves-light">Registrarme</a></li>
       </ul>
     </ul>
 
     <ul class="side-nav" id="mobile">
-      <li><a href="#" class="waves-effect waves-light">About</a></li>
-      <li><a href="#" class="waves-effect waves-light">Features</a></li>
-      <li><a href="#" class="waves-effect waves-light">Learn</a></li>
+      <li><a href="#" class="waves-effect waves-light">Explorar</a></li>
+      <li><a href="#" class="waves-effect waves-light">¿Cómo ser Anfitrión?</a></li>
+      <li><a href="#" class="waves-effect waves-light">Iniciar Sesión</a></li>
+      <li><a href="#" class="waves-effect waves-light">Registrarme</a></li>
     </ul>
 
   </div>
 </nav>
 <!-- NAVBAR -->
+<main>
+</main>
+</body>
 @stop
