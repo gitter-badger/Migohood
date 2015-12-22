f86d3febfdd0f8e1cdbe2c7d4344dae18e0335b1
user <yefferson.marin@gmail.com>
2015-12-22 02:14:31 -0500

    Media Helpers and Responsive improvemments

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -10,8 +10,43 @@
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">    <!-- Material Icons -->
       @yield('header')
   </head>
+
+<!-- Navbar -->
+<header>
+  <nav class="white" role="navigation">
+    <div class="nav-wrapper">
+      <a href="#" data-activates="mobile" class="button-collapse"><i class="material-icons">dehaze</i></a>
+
+      <ul class="logo">
+        <li><a href="{{ url('/') }}"><img id="img" src="/img/brand.png" alt=".."/></a></li>
+      </ul>
+
+      <ul class="right">
+        <li><a href="" >Explorar</a></li>
+        <li><a href="" >¿Cómo ser Anfitrión?</a></li>
+        <li><a href="{{ url('') }}" class="btn btn-primary">Iniciar Sesión</a></li>
+        <li><a href="{{ url('') }}" class="btn btn-secundary waves-effect waves-light">Registrarme</a></li>
+      </ul>
+
+      <ul class="side-nav" id="mobile">
+        <li><a href="#" class="waves-effect waves-light">Explorar</a></li>
+        <li><a href="#" class="waves-effect waves-light">¿Cómo ser Anfitrión?</a></li>
+        <li><a href="#" class="waves-effect waves-light">Iniciar Sesión</a></li>
+        <li><a href="#" class="waves-effect waves-light">Registrarme</a></li>
+      </ul>
+
+    </div>
+  </nav>
+</header>
+<!-- Navbar -->
+
+<body>
+<main>
 @yield('content')
+</main>
+</body>
 </html>
+
 <script src="/js/jquery.min.js" type="text/javascript"></script>                <!-- Jquery core JS -->
 <script src="/js/materialize.min.js" type="text/javascript"></script>           <!-- Materialize core JS -->
 <script src="/js/init.js" type="text/javascript"></script>                      <!-- Init Custom JS File -->
--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -4,34 +4,7 @@
   <link href="/css/site.css" rel="stylesheet">    <!-- Style core CSS -->
 @stop
 @section('content')
-<body id="home">
-<!-- NAVBAR -->
-<nav class="white" role="navigation">
-  <div class="nav-wrapper">
-    <!--<a href="#" data-activates="mobile" class="button-collapse"><i class="material-icons">dehaze</i></a>-->
-    <ul class="hide-on-med-and-down">
-      <ul class="left">
-        <li><a href="{{ url('/') }}"><img id="brand" src="/img/brand.png" alt=".."/></a></li>
-      </ul>
-      <ul class="right">
-        <li><a href="" >Explorar</a></li>
-        <li><a href="" >¿Cómo ser Anfitrión?</a></li>
-        <li><a href="{{ url('') }}" class="btn btn-primary">Iniciar Sesión</a></li>
-        <li><a href="{{ url('') }}" class="btn btn-secundary waves-effect waves-light">Registrarme</a></li>
-      </ul>
-    </ul>
+<section id="welcome">
 
-    <ul class="side-nav" id="mobile">
-      <li><a href="#" class="waves-effect waves-light">Explorar</a></li>
-      <li><a href="#" class="waves-effect waves-light">¿Cómo ser Anfitrión?</a></li>
-      <li><a href="#" class="waves-effect waves-light">Iniciar Sesión</a></li>
-      <li><a href="#" class="waves-effect waves-light">Registrarme</a></li>
-    </ul>
-
-  </div>
-</nav>
-<!-- NAVBAR -->
-<main>
-</main>
-</body>
+</section>
 @stop
