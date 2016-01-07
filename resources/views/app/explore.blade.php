7187794085ecb698c6f257427ae433bf1a544087
user <yefferson.marin@gmail.com>
2016-01-07 05:57:56 -0500

    Explore inital Design ready

--- resources/views/app/explore.blade.php
+++ resources/views/app/explore.blade.php
@@ -1,7 +1,36 @@
 @extends('layouts.app')
 @section('title', 'Explore')
+@section('header')
+  <link href="/css/explore.css" rel="stylesheet">   <!-- Custom core CSS -->
+@stop
 @section('content')
 
+<!-- Content-->
+<section class="row" id="explore">
+  <div class="col s3 search">
+    
+  </div>
+  <div class="col s9 row">
+     @include('common.announces')
+
+     <!-- pagination -->
+     <div class="center">
+       <ul class="pagination">
+         <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+         <li class="active"><a href="#!">1</a></li>
+         <li class="waves-effect"><a href="#!">2</a></li>
+         <li class="waves-effect"><a href="#!">3</a></li>
+         <li class="waves-effect"><a href="#!">4</a></li>
+         <li class="waves-effect"><a href="#!">5</a></li>
+         <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+       </ul>
+     </div>
+     <!-- pagination -->
+  </div>
+
+</div>
+</section>
+<!-- Content-->
 <!--
 Hello from Explore
 
---
+++ resources/views/common/announces.blade.php
@@ -0,0 +1,79 @@
+<!-- Debuggin -->
+<div class="col s4 announce-box">
+  <div class="announce-body">
+
+    <div class="announce-pic">
+    <img src="/img/Test/1.jpg">
+    </div>
+
+    <div class="announce-title">
+      <img src="/img/Faces/1.jpg">
+    </div>
+
+  </div>
+</div>
+
+<div class="col s4 announce-box">
+  <div class="announce-body">
+
+    <div class="announce-pic">
+    <img src="/img/Test/2.jpg">
+    </div>
+
+    <div class="announce-title">
+      <img src="/img/Faces/2.jpg" class="circle">
+    </div>
+  </div>
+</div>
+
+<div class="col s4 announce-box">
+  <div class="announce-body">
+
+    <div class="announce-pic">
+    <img src="/img/Test/3.jpg">
+    </div>
+
+    <div class="announce-title">
+      <img src="/img/Faces/3.jpg" class="circle">
+    </div>
+  </div>
+</div>
+
+<div class="col s4 announce-box">
+  <div class="announce-body">
+
+    <div class="announce-pic">
+    <img src="/img/Test/4.jpg">
+    </div>
+
+    <div class="announce-title">
+      <img src="/img/Faces/4.jpg" class="circle">
+    </div>
+  </div>
+</div>
+
+<div class="col s4 announce-box">
+  <div class="announce-body">
+
+    <div class="announce-pic">
+    <img src="/img/Test/5.jpg">
+    </div>
+
+    <div class="announce-title">
+      <img src="/img/Faces/5.jpg" class="circle">
+    </div>
+  </div>
+</div>
+
+<div class="col s4 announce-box">
+  <div class="announce-body">
+
+    <div class="announce-pic">
+    <img src="/img/Test/6.jpg">
+    </div>
+
+    <div class="announce-title">
+      <img src="/img/Faces/6.jpg" class="circle">
+    </div>
+  </div>
+</div>
